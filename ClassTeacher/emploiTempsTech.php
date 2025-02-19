bc1e672d67853fb1753b28dff7abaa8b0e74b26e
HAFSA-ELOMARI <elomarihafsa203@gmail.com>
2025-02-19 09:55:26 +0100

    Add files via upload

--- ClassTeacher/emploiTempsTech.php
+++ ClassTeacher/emploiTempsTech.php
@@ -4,21 +4,12 @@ ini_set('display_errors', 1);
 include '../Includes/dbcon.php';
 include '../Includes/session.php';
 
-// التأكد من أن المستخدم هو أستاذ
-if (!isset($_SESSION['teacherId'])) {
-    header("Location: ../login.php");
-    exit();
-}
+// جلب معرف الأستاذ من الجلسة
+$teacherId = $_SESSION['userId'];
 
-$teacherId = $_SESSION['teacherId']; // معرف الأستاذ
-
-// جلب معلومات الأستاذ
-$teacherQuery = mysqli_query($conn, "SELECT firstName, lastName FROM tblclassteacher WHERE id = '$teacherId'");
-$teacherData = mysqli_fetch_assoc($teacherQuery);
-
-// جلب استعمال الزمن للأستاذ
+// جلب استعمال الزمن للأستاذ بناءً على المواد التي يدرسها
 $scheduleQuery = mysqli_query($conn, "
-    SELECT s.day, t.termName, c.className, ca.classArmName, s.subject 
+    SELECT s.id, s.day, t.termName, c.className, ca.classArmName, s.subject
     FROM tblschedule s
     JOIN tblterm t ON s.termId = t.id
     JOIN tblclass c ON s.classId = c.id
@@ -27,57 +18,92 @@ $scheduleQuery = mysqli_query($conn, "
     ORDER BY s.day, s.classId, s.classArmId
 ");
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Emploi du Temps</title>
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="img/logo/attnlg.jpg" rel="icon">
+    <?php include 'includes/title.php'; ?>
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="css/ruang-admin.min.css" rel="stylesheet">
 </head>
 
-<body>
-    <div class="container mt-5">
-        <h2 class="text-center">Emploi du Temps</h2>
+<body id="page-top">
+    <div id="wrapper">
+        <?php include "Includes/sidebar.php"; ?>
+        <div id="content-wrapper" class="d-flex flex-column">
+            <div id="content">
+                <?php include "Includes/topbar.php"; ?>
+                <div class="container-fluid">
 
-        <!-- معلومات الأستاذ -->
-        <div class="card mb-4">
-            <div class="card-body">
-                <h5 class="card-title">Informations de l'Enseignant</h5>
-                <p><strong>Nom :</strong> <?php echo $teacherData['firstName'] . " " . $teacherData['lastName']; ?></p>
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    </div>
+
+                    <!-- Container Fluid-->
+                    <div class="container-fluid" id="container-wrapper">
+                        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                            <h1 class="h3 mb-0 text-gray-800">Emploi du Temps</h1>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="./">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Emploi du Temps </li>
+                            </ol>
+                        </div>
+
+                        <!-- جدول الامتحانات -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="card mb-4">
+                                    <div class="card-header py-3">
+                                        <h6 class="m-0 font-weight-bold text-primary">Emploi du Temps </h6>
+                                    </div>
+                                    <div class="table-responsive p-3">
+                                        <table class="table table-bordered">
+                                            <thead class="thead-light">
+                                                <tr>
+                                                    <th>#</th>
+                                                    <th>Jour</th>
+                                                    <th>Trimestre</th>
+                                                    <th>Classe</th>
+                                                    <th>Section</th>
+                                                    <th>Matière</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $sn = 1;
+                                                while ($row = mysqli_fetch_assoc($scheduleQuery)) {
+                                                    echo "<tr>
+                                                               <td>{$sn}</td>
+                                                               <td>{$row['day']}</td>
+                                                               <td>{$row['termName']}</td>
+                                                               <td>{$row['className']}</td>
+                                                               <td>{$row['classArmName']}</td>
+                                                               <td>{$row['subject']}</td>
+                                                             </tr>";
+                                                    $sn++;
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <?php include "Includes/footer.php"; ?>
             </div>
         </div>
 
-        <!-- جدول استعمال الزمن -->
-        <table class="table table-bordered table-striped">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Jour</th>
-                    <th>Trimestre</th>
-                    <th>Classe</th>
-                    <th>Section</th>
-                    <th>Matière</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php while ($row = mysqli_fetch_assoc($scheduleQuery)) { ?>
-                    <tr>
-                        <td><?php echo $row['day']; ?></td>
-                        <td><?php echo $row['termName']; ?></td>
-                        <td><?php echo $row['className']; ?></td>
-                        <td><?php echo $row['classArmName']; ?></td>
-                        <td><?php echo $row['subject']; ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
-
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
